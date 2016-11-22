19e189d62165df20f2584cd67e4eb59d838d3426
Carlan Calazans <carlancalazans@gmail.com>
2016-11-22 23:08:20 +0000

    [python] Flask
    - Update passpass.py to return a random number

--- python/flask/passpass.py
+++ python/flask/passpass.py
@@ -1,10 +1,14 @@
 from flask import Flask, render_template, url_for, render_template
+import random
 
 app = Flask(__name__)
+app.config['DEBUG'] = True
+app.config['TEMPLATES_AUTO_RELOAD'] = True
 
 @app.route('/', methods=['GET'])
 def index():
-    return render_template('index.html')
+    password = random.random()
+    return render_template('index.html', password=password)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
