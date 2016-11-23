8dea514989e061618557c4c1e5cbd46209310c57
Carlan Calazans <carlancalazans@gmail.com>
2016-11-23 22:06:19 +0000

    [python] Flask
    - Added file header

--- python/flask/passpass.py
+++ python/flask/passpass.py
@@ -1,3 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""passpass.py: Password Generator"""
+__author__ = "Carlan Calazans"
+__copyright__ = "Copyright 2016, Carlan Calazans"
+__credits__ = ["Carlan Calazans"]
+__license__ = "MIT"
+__version__ = "1.0.0"
+__maintainer__ = "Carlan Calazans"
+__email__ = "carlancalazans at gmail dot com"
+__status__ = "Development"
+
 from flask import Flask, render_template, url_for, render_template
 import random
 
