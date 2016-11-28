ae0adb0d542850d4c607cfc47b7d4a1a29e44ed3
Carlan Calazans <carlancalazans@gmail.com>
2016-11-28 21:36:42 +0000

    [java] Spring
    - Updated gitignore
    - Added java spring version

---
+++ java/spring/passpass/src/main/java/io/github/carlan/passpass/PasspassApplication.java
@@ -0,0 +1,12 @@
+package io.github.carlan.passpass;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PasspassApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PasspassApplication.class, args);
+	}
+}
